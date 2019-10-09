5c16c2e8f900851a26e956e5768e901b874032eb
Stephen McMillen <Shtevetm@gmail.com>
2019-10-09 13:57:07 -0500

    formatiing revision

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require("express");
 var path = require("path");
 
-// Sets up the Express App
+// Sets up the Express App 
 // =============================================================
 var app = express();
 var PORT = 3000;
